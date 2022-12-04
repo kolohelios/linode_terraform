9fcf9b2603bec0e48b287c5270f2751a2c0d80a5
Jon Edwards <jkedwards@me.com>
2022-12-03 21:09:25 -0800

    add kubernetes provider

--- main.tf
+++ main.tf
@@ -4,10 +4,14 @@ terraform {
             source  = "linode/linode"
             version = "1.26.0"
         }
+        kubernetes = {
+            source  = "hashicorp/kubernetes"
+            version = ">= 2.0.0"
+        }
     }
 }
 
-resource "linode_lke_cluster" "kolohelios" {
+resource "linode_lke_cluster" "kolohelios_cluster" {
     label       = "kolohelios"
     k8s_version = "1.24"
     region      = "us-west"
@@ -29,4 +33,13 @@ resource "linode_lke_cluster" "kolohelios" {
         pool.0.count
     ]
     }
+}
+
+resource "local_file" "kube_config" {
+    content  = base64decode(linode_lke_cluster.kolohelios_cluster.kubeconfig)
+    filename = "kubeconfig"
+}
+
+provider "kubernetes" {
+    config_path = "kubeconfig"
 }
